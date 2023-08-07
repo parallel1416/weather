8ca7f262bb2aefeef500cd47d1545c2d21496aa2
Guanyu Zhang <108467515+TonyZhang2004@users.noreply.github.com>
2023-08-07 22:05:07 +0800

    convert histogram d3 code into React-usable component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import clsx from "clsx";
-import {makeStyles} from "@material-ui/core";
 import MainView from "./MainView";
 import ControlPanel from "./ControlPanel";
 import DetailView from "./DetailView";
--- src/histogram.js
+++ src/histogram.js
@@ -1,147 +1,160 @@
-////////////////basic layout//////////////////
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+import React, { useRef, useEffect } from "react";
+import * as d3 from 'd3' 
 
-var svg = d3.select("#histogram")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+const Histogram = (props) => {
+    const svgRef = useRef(null);
 
-d3.csv("./sort_by_tempmax.csv", function(dataset) {
+    useEffect(() => {
 
-    //////////preprocess the dataset////////////
-    const length = 3867;
-    var THRESHOLD_RATIO = 0.1;
-    var countObj = {};
+        ////////////////basic layout//////////////////
+        var margin = {top: 30, right: 30, bottom: 70, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
-    filtered_data = dataset.filter(function(d, i){return i < THRESHOLD_RATIO * length;})
+        var svg = d3.select("#root")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-    filtered_data.forEach(function(d) {
-        var year = d.datetime;
-        if(countObj[year] === undefined) {
-            countObj[year] = 0;
-        } else {
-            countObj[year] = countObj[year] + 1;
-        }
-    });
- 
-    filtered_data.forEach(function(d) {
-        var year = d.datetime;
-        d.count = countObj[year];
-    });
+        d3.csv("sort_by_tempmax.csv").then(dataset => { //This line is IMPORTANT!!! converts the data to a PROMISE
 
-
-    // sort data
-    filtered_data.sort(function(b, a) {
-    return a.count - b.count;
-  });
-
-  // X axis
-  var x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(filtered_data.map(function(d) { return d.datetime; }))
-    .padding(0.2);
-  xAxis = svg.append("g");
-    xAxis.attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-      .style("text-anchor", "middle");
-
-  // Add Y axis
-  var y = d3.scaleLinear()
-    .domain([0, d3.max(filtered_data, function(d) {return d.count;})])
-    .range([ height, 0]);
-  yAxis = svg.append("g");
-    yAxis.call(d3.axisLeft(y));
-
-  // Bars
-  svg.selectAll("rect")
-    .data(filtered_data)
-    .enter()
-    .append("rect")
-    .attr("x", function(d) { return x(d.datetime); })
-    .attr("y", function(d) { return y(d.count); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d.count); })
-    .attr("fill", "#d3d3d3")
-
-    svg
-      .append("text")
-      .attr("text-anchor", "start")
-      .attr("y", -10)
-      .attr("x", -30)
-      .text(function(d){ return('Days')})
-    
-    
-    //////////////////////////////////////////////
-    d3.select("#mySlider").on("change", function(d) {
-        update(this.value)
-    })
-
-    function update(threshold){
+        //////////preprocess the dataset////////////
+        const length = 3867;
+        var THRESHOLD_RATIO = 0.1;
         var countObj = {};
 
-        filtered_data = dataset.filter(function(d, i){return i < threshold * length / 1000;})
+        var filtered_data = dataset.filter(function(d, i){return i < THRESHOLD_RATIO * length;})
 
         filtered_data.forEach(function(d) {
-        var year = d.datetime;
-        if (countObj[year] === undefined) {
-            countObj[year] = 1;
-        } else {
-            countObj[year] = countObj[year] + 1;
-        }
+            var year = d.datetime;
+            if(countObj[year] === undefined) {
+                countObj[year] = 0;
+            } else {
+                countObj[year] = countObj[year] + 1;
+            }
         });
- 
+
         filtered_data.forEach(function(d) {
-        var year = d.datetime;
-        d.count = countObj[year];
+            var year = d.datetime;
+            d.count = countObj[year];
         });
 
 
         // sort data
         filtered_data.sort(function(b, a) {
-            return a.count - b.count;
+        return a.count - b.count;
         });
 
-
-        // Update X axis
+        // X axis
         var x = d3.scaleBand()
-            .range([ 0, width ])
-            .domain(filtered_data.map(function(d) { return d.datetime; }))
-            .padding(0.2);
-        xAxis.transition()
-        .duration(1000)
-        .call(d3.axisBottom(x));
+        .range([ 0, width ])
+        .domain(filtered_data.map(function(d) { return d.datetime; }))
+        .padding(0.2);
+        var xAxis = svg.append("g");
+        xAxis.attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .style("text-anchor", "middle");
 
-        // Update Y axis
-        y.domain([0, d3.max(filtered_data, function(d) {return d.count;})]);
-        yAxis.transition()
-        .duration(1000)
-        .call(d3.axisLeft(y));
+        // Add Y axis
+        var y = d3.scaleLinear()
+        .domain([0, d3.max(filtered_data, function(d) {return d.count;})])
+        .range([ height, 0]);
+        var yAxis = svg.append("g");
+        yAxis.call(d3.axisLeft(y));
 
         // Bars
-        tmp = svg.selectAll("rect")
-            .data(filtered_data);
-
-        tmp.exit().remove()
-
-        
-        tmp.enter()
+        svg.selectAll("rect")
+        .data(filtered_data)
+        .enter()
         .append("rect")
-        .merge(tmp)
-        .transition()
-        .duration(1000)
         .attr("x", function(d) { return x(d.datetime); })
         .attr("y", function(d) { return y(d.count); })
         .attr("width", x.bandwidth())
         .attr("height", function(d) { return height - y(d.count); })
-        .attr("fill", "#d3d3d3")
+        .attr("fill", "#08def1")
 
-        
-    }
-    
-});
+        svg
+        .append("text")
+        .attr("text-anchor", "start")
+        .attr("y", -10)
+        .attr("x", -30)
+        .text(function(d){ return('Days')})
+
+
+        //////////////////////////////////////////////
+        d3.select("#mySlider").on("change", function(d) {
+            update(this.value)
+        })
+
+        function update(threshold){
+            var countObj = {};
+
+            filtered_data = dataset.filter(function(d, i){return i < threshold * length / 1000;})
+
+            filtered_data.forEach(function(d) {
+            var year = d.datetime;
+            if (countObj[year] === undefined) {
+                countObj[year] = 1;
+            } else {
+                countObj[year] = countObj[year] + 1;
+            }
+            });
+
+            filtered_data.forEach(function(d) {
+            var year = d.datetime;
+            d.count = countObj[year];
+            });
+
+
+            // sort data
+            filtered_data.sort(function(b, a) {
+                return a.count - b.count;
+            });
+
+
+            // Update X axis
+            var x = d3.scaleBand()
+                .range([ 0, width ])
+                .domain(filtered_data.map(function(d) { return d.datetime; }))
+                .padding(0.2);
+            xAxis.transition()
+            .duration(1000)
+            .call(d3.axisBottom(x));
+
+            // Update Y axis
+            y.domain([0, d3.max(filtered_data, function(d) {return d.count;})]);
+            yAxis.transition()
+            .duration(1000)
+            .call(d3.axisLeft(y));
+
+            // Bars
+            var tmp = svg.selectAll("rect")
+                .data(filtered_data);
+
+            tmp.exit().remove()
+
+            
+            tmp.enter()
+            .append("rect")
+            .merge(tmp)
+            .transition()
+            .duration(1000)
+            .attr("x", function(d) { return x(d.datetime); })
+            .attr("y", function(d) { return y(d.count); })
+            .attr("width", x.bandwidth())
+            .attr("height", function(d) { return height - y(d.count); })
+            .attr("fill", "#08def1")    
+        }
+        });
+
+     }, [props.Data, svgRef.current]);
+
+    return <svg ref={svgRef} />;
+
+};
+
+export default Histogram;
