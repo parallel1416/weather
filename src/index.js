83b1065300007abc35c2af3de2dafffba0bbd9e3
Ing Ventaka <110343080+parallel1416@users.noreply.github.com>
2023-08-09 11:03:12 +0800

    merge changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import MainView from './MainView';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <MainView />
-);
-
-reportWebVitals();
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -21,10 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 reportWebVitals();
->>>>>>> Stashed changes
