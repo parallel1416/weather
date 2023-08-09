7ba7faa1f9a6ce188cc4a13f4a7697d6a4f93922
Ing Ventaka <110343080+parallel1416@users.noreply.github.com>
2023-08-09 10:59:54 +0800

    resolve stashed changes

--- src/MainView.js
+++ src/MainView.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React, {useContext} from 'react';
 import Histogram from "./histogram";
 import LineChart from './line_chart';
@@ -12,16 +11,4 @@ function MainView(){
         </>);
 }
 
-=======
-import React, {useContext} from 'react';
-import Histogram from "./histogram";
-import Slider from '@mui/material/Slider';
-
-const MainView=(startyear, endyear, dimension)=>{
-    return (<>
-        <div><Histogram/></div>
-        </>);
-}
-
->>>>>>> Stashed changes
 export default MainView;
